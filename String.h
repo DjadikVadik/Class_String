70df1bba2d41abab8e90267e476c7a87169af847
Vadim <www.220391@gmail.com>
2022-06-23 22:01:16 +0300

    operator

--- String.h
+++ String.h
@@ -25,6 +25,7 @@ public:
 	bool operator ==(const String& other);
 	bool operator !=(const String& other);
 	bool operator ==(const char* text);
+	bool operator !=(const char* text);
 	bool operator<(const String& other);
 	bool operator<(const char* text);
 	bool operator>(const String& other);
